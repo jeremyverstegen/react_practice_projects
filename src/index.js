6a0b3ec953222f7f963531ef8dab0ee520d91328
Jeremy Verstegen <jvmedia77@gmail.com>
2022-08-13 17:21:10 -0600

    props

--- src/index.js
+++ src/index.js
@@ -3,26 +3,47 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/81LFAmKY4FL._AC_UY218_.jpg',
+  title: 'Where the Crawdads Sing',
+  author: 'Delia Owens'
+};
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/91QA8vBmgbL._AC_UY218_.jpg',
+  title: 'Exiles',
+  author: 'Ashley Saunders & Leslie Saunders'
+};
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}
+      >
+        <p>Lorem Ipsum</p>
+      </Book>
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author} 
+      />
     </section>
   );
 };
 
-const author = 'Delia Owens';
-
-const Book = () => {
-  const title = 'Where the Crawdads Sing';
+const Book = (props) => {
+  const { img, title, author, children } = props;
+  console.log(props);
   return (
     <article className='book'>
-        <img src='https://m.media-amazon.com/images/I/41P9cpPL0gL._SY346_.jpg' alt='book' />
+        <img src={img} alt='' />
         <h1>{title}</h1>
         <h4>{author}</h4>
+        {children}
     </article>
-  )
+  );
 };
 
 
