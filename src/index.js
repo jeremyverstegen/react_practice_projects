3d7eee9f29bb08b6d5b9b1d52ab2b9cf43bf1672
Jeremy Verstegen <jvmedia77@gmail.com>
2022-08-13 17:48:09 -0600

    spread books object

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     img: 'https://m.media-amazon.com/images/I/81LFAmKY4FL._AC_UY218_.jpg',
     title: 'Where the Crawdads Sing',
     author: 'Delia Owens'
   },
   {
+    id: 2,
     img: 'https://m.media-amazon.com/images/I/91QA8vBmgbL._AC_UY218_.jpg',
     title: 'Exiles',
     author: 'Ashley Saunders & Leslie Saunders'
   },
   {
+    id: 3,
     img: 'https://m.media-amazon.com/images/I/71HuyKCcs4L._AC_UY218_.jpg',
     title: 'The Seven Husbands of Evelyn Hugo',
     author: 'Taylor Jenkins Reid'
@@ -25,15 +28,14 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book book={book} /> 
+        return <Book key={book.id} {...book} /> 
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
 
   return (
     <article className='book'>
