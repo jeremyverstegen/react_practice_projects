17bd3db4d8a4fe50b25b28d0f7387e7680761566
Jeremy Verstegen <jvmedia77@gmail.com>
2022-08-13 17:40:32 -0600

    books array

--- src/index.js
+++ src/index.js
@@ -3,45 +3,43 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/81LFAmKY4FL._AC_UY218_.jpg',
-  title: 'Where the Crawdads Sing',
-  author: 'Delia Owens'
-};
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/91QA8vBmgbL._AC_UY218_.jpg',
-  title: 'Exiles',
-  author: 'Ashley Saunders & Leslie Saunders'
-};
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/81LFAmKY4FL._AC_UY218_.jpg',
+    title: 'Where the Crawdads Sing',
+    author: 'Delia Owens'
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/91QA8vBmgbL._AC_UY218_.jpg',
+    title: 'Exiles',
+    author: 'Ashley Saunders & Leslie Saunders'
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/71HuyKCcs4L._AC_UY218_.jpg',
+    title: 'The Seven Husbands of Evelyn Hugo',
+    author: 'Taylor Jenkins Reid'
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author}
-      >
-        <p>Lorem Ipsum</p>
-      </Book>
-      <Book 
-        img={secondBook.img} 
-        title={secondBook.title} 
-        author={secondBook.author} 
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book book={book} /> 
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
-  console.log(props);
+  const { img, title, author } = props.book;
+
   return (
     <article className='book'>
         <img src={img} alt='' />
         <h1>{title}</h1>
         <h4>{author}</h4>
-        {children}
     </article>
   );
 };
