5035fed5d2a0ff06d30b95ec3aceada941362591
Jeremy Verstegen <jvmedia77@gmail.com>
2022-08-13 16:41:31 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+    </section>
+  );
+};
+
+const author = 'Delia Owens';
+
+const Book = () => {
+  const title = 'Where the Crawdads Sing';
+  return (
+    <article className='book'>
+        <img src='https://m.media-amazon.com/images/I/41P9cpPL0gL._SY346_.jpg' alt='book' />
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+    </article>
+  )
+};
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
